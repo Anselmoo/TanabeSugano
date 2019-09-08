2e874da52e9c40968e652b2b7e288ead8d96775a
Anselmoo <Anselm.Hahn@gmail.com>
2019-09-08 16:27:57 +0200

    Updating the setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+__authors__ = ['hahn']
+__license__ = 'MIT'
+__date__ = '08/09/2019'
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
@@ -9,10 +12,11 @@ setup(
 	version='0.5',
 	packages=['tanabe','test'],
 	install_requires=required,
-	url='',
-	license='MIT',
-	author='hahn',
+	url='https://github.com/Anselmoo/TanabeSugano',
+	download_url='https://github.com/Anselmoo/TanabeSugano/releases',
+	license=__license__,
+	author=__authors__,
 	author_email='Anselm.Hahn@gmail.com',
 	description='A python-solver for Tanabe-Sugano and energy-correlation diagrams',
-	platforms=['MacOS :: MacOS X', 'Microsoft :: Windows','POSIX :: Linux']
+	platforms=['MacOS :: MacOS X', 'Microsoft :: Windows','POSIX :: Linux'],
 )
