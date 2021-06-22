d79e31b316ab53476670c8942b03d237ec873401
Anselm Hahn <Anselm.Hahn@gmail.com>
2021-06-22 20:58:17 +0200

    Update tests

--- tanabesugano/test/test_batch.py
+++ tanabesugano/test/test_batch.py
@@ -8,42 +8,42 @@ def test_batch_small():
 
 
 def test_batch_d2():
-    res = Batch(mode=2)
+    res = Batch(d_count=2)
     res.calculation()
     assert isinstance(res.result, list)
 
 
 def test_batch_d3():
-    res = Batch(mode=3)
+    res = Batch(d_count=3)
     res.calculation()
     assert isinstance(res.result, list)
 
 
 def test_batch_d4():
-    res = Batch(mode=4)
+    res = Batch(d_count=4)
     res.calculation()
     assert isinstance(res.result, list)
 
 
 def test_batch_d5():
-    res = Batch(mode=5)
+    res = Batch(d_count=5)
     res.calculation()
     assert isinstance(res.result, list)
 
 
 def test_batch_d6():
-    res = Batch(mode=6)
+    res = Batch(d_count=6)
     res.calculation()
     assert isinstance(res.result, list)
 
 
 def test_batch_d7():
-    res = Batch(mode=7)
+    res = Batch(d_count=7)
     res.calculation()
     assert isinstance(res.result, list)
 
 
 def test_batch_d8():
-    res = Batch(mode=8)
+    res = Batch(d_count=8)
     res.calculation()
     assert isinstance(res.result, list)
--- tanabesugano/test/test_front.py
+++ tanabesugano/test/test_front.py
@@ -3,6 +3,6 @@ from tanabesugano import __main__ as frontapp
 
 def test_frontapp():
     return frontapp.CMDmain(
-        Dq=4000.0, B=400.0, C=3600.0, nroots=100, mode=5
+        Dq=4000.0, B=400.0, C=3600.0, nroots=100, d_count=5
     ).calculation()
     assert True
